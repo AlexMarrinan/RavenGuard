a968e0982799da8bf8b96fea6dac318986af7815
Alex Marrinan <alex@balroe.com>
2023-11-14 22:48:25 -0500

    fixed errors when building

--- Assets/Scripts/Skills/Buffs/DualityBuff.cs
+++ Assets/Scripts/Skills/Buffs/DualityBuff.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class DualityBuff : Buff{
