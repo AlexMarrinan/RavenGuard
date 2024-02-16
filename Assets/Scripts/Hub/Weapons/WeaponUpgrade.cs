049ebf1f1a48bcc23e35faae6720269b36810cb3
Tate Donnelly <58042977+Tate-Donnelly@users.noreply.github.com>
2024-02-16 00:21:49 -0500

    Weapon Upgrade prefab

--- Assets/Scripts/Hub/Weapons/WeaponUpgrade.cs
+++ Assets/Scripts/Hub/Weapons/WeaponUpgrade.cs
@@ -8,16 +8,23 @@ namespace Hub.Weapons
     public class WeaponUpgrade:MonoBehaviour
     {
         [SerializeField] private List<StatUI> stats = new List<StatUI>();
-        [SerializeField] private TextMeshProUGUI name;
+        [SerializeField] private TextMeshProUGUI weaponName;
+        [SerializeField] private TextMeshProUGUI rarity;
         [SerializeField] private TextMeshProUGUI desc;
+        
+        [SerializeField] private GameObject costPanel;
         [SerializeField] private TextMeshProUGUI cost;
+
+        private bool weaponUnlocked;
         
         public void Init(WeaponType weapon)
         {
-            name.text = weapon.weapon.weaponName;
+            weaponName.text = weapon.weapon.weaponName;
+            rarity.text = "Common";
             desc.text = weapon.weapon.weaponDescription;
             cost.text = weapon.cost.ToString();
             LoadStats(weapon.weapon);
+            SetUnlocked(false);
         }
 
         private void LoadStats(BaseWeapon weapon)
@@ -31,6 +38,22 @@ namespace Hub.Weapons
                     stats[i].Init(statIcons[i]);
                 }
             }
+        }
+
+        public void UnlockWeapon()
+        {
+            // TODO: Check if player has enough money
+            if (true)
+            {
+                SetUnlocked(true);
+                //TODO: Tell save system that this weapon has been unlocked
+            }
+        }
+
+        private void SetUnlocked(bool unlockedStatus)
+        {
+            weaponUnlocked = unlockedStatus;
+            costPanel.SetActive(unlockedStatus);
         }
     }
 }
