8a6521c85cd07dfd2b27182a15b9322ff646e13d
Alex Marrinan <alex@balroe.com>
2024-04-18 21:37:38 -0400

    fixed knight attack animation controller again (?)

--- Assets/Scripts/OverworldMap/OverworldMapManager.cs
+++ Assets/Scripts/OverworldMap/OverworldMapManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Game.Dialogue;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
